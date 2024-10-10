cb5135ba791e6e69e3b45ccfd810d507720794ec
CaioMBM <caiombmonteiro@gmail.com>
2024-10-10 15:13:57 -0300

    Adição do JAVASCRIPT

---
+++ modal.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("modal")
+
+/* Exibir o modal */
+function showModal(){
+    modal.classList.add('showModal');  
+    modal.classList.remove('closeModal');  
+
+    modal.style.display = 'block'; // Certifica que o modal está visível
+}
+
+/* Esconder o modal */
+function closeModal(){
+    modal.classList.add('closeModal');  
+    modal.classList.remove('showModal');  
+
+    // Usar setTimeout() para esperar a animação terminar antes de esconder
+    setTimeout(function() {
+        modal.style.display = 'none'; // Esconde o modal após a animação
+    }, 1000); // O tempo deve corresponder à duração da animação (.closeModal 1s)
+}
